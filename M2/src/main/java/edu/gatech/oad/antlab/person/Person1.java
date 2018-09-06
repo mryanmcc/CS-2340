14483581b6dce685ea338b107e3f29069f042483
Yun <yjeon43@gatech.edu>
2018-09-06 19:26:32 -0400

    Changed person 1

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Yun
  *  @version 1.1
  */
 public class Person1 {
@@ -17,7 +17,9 @@ public class Person1 {
 	 * @param pname the person's real name
 	 */
   public Person1(String pname) {
-    name = pname;
+
+  	name = pname;
+
   }
   	/**
 	 * This method should take the string
@@ -31,7 +33,7 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+	  return input;
 	}
 	
 	/**
@@ -43,7 +45,8 @@ public class Person1 {
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+
+		return name + calc(input);
 	}
 
 }
