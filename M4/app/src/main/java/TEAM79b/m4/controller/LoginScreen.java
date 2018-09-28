ba945eb661dc1dbb73444b3e364b11e7ea443f00
rwturko <rwturko@gmail.com>
2018-09-28 00:13:41 -0400

    fixed login authentication

--- M4/app/src/main/java/TEAM79b/m4/controller/LoginScreen.java
+++ M4/app/src/main/java/TEAM79b/m4/controller/LoginScreen.java
@@ -5,26 +5,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import TEAM79b.m4.R;
+import TEAM79b.m4.model.Login;
 
 public class LoginScreen extends AppCompatActivity {
+
+    private EditText loginEmail;
+    private EditText loginPass;
+    private Button loginSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
-        goToApp();
+        loginEmail = (EditText) findViewById(R.id.loginEmail);
+        loginPass = (EditText) findViewById(R.id.loginPass);
+        loginSubmit = (Button) findViewById(R.id.loginSubmit);
+
+//        goToApp();
     }
 
-    private void goToApp() {
-        Button logInSubmit = (Button) findViewById(R.id.logInSubmit);
-        logInSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(LoginScreen.this, AppScreen.class));
-            }
-        });
+    public void loginPress(View view) {
+        Login model = Login.getInstance();
+        if (model.getUserAccounts().containsKey(loginEmail.getText().toString()) && model.getUserAccounts().get(loginEmail.getText().toString()).equals(loginPass.getText().toString())) {
+            startActivity(new Intent(LoginScreen.this, AppScreen.class));
+        } else {
+            startActivity(new Intent(LoginScreen.this, LoginScreen.class));
+        }
     }
+//    private void goToApp() {
+//        loginSubmit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(LoginScreen.this, AppScreen.class));
+//            }
+//        });
+//    }
 }
--- M4/app/src/main/java/TEAM79b/m4/controller/RegisterScreen.java
+++ M4/app/src/main/java/TEAM79b/m4/controller/RegisterScreen.java
@@ -5,26 +5,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import TEAM79b.m4.R;
+import TEAM79b.m4.model.Login;
 
 public class RegisterScreen extends AppCompatActivity {
+
+    private EditText regEmail;
+    private EditText regPass;
+    private Button regSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
 
-        goToApp();
+        regEmail = (EditText) findViewById(R.id.regEmail);
+        regPass = (EditText) findViewById(R.id.regPass);
+        regSubmit = (Button) findViewById(R.id.regSubmit);
+
+        //goToApp();
     }
 
-    private void goToApp() {
-        Button regSubmit = (Button) findViewById(R.id.regSubmit);
-        regSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(RegisterScreen.this, AppScreen.class));
-            }
-        });
+//    private void goToApp() {
+//        regSubmit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(RegisterScreen.this, AppScreen.class));
+//            }
+//        });
+//    }
+
+    public void regPress(View view) {
+        Login model = Login.getInstance();
+        model.addUser(regEmail.getText().toString(), regPass.getText().toString());
+        startActivity(new Intent(RegisterScreen.this, AppScreen.class));
     }
 }
