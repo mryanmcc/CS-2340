53c1737e2978ea7975fbbfed6c6763acf783ee81
mryanmcc <mryanmcc@gmail.com>
2018-10-04 16:24:23 -0400

    adjusting failed login to a popup, and adding role spinner to registration

--- M4/app/src/main/java/TEAM79b/m4/controller/BadIdScreen.java
+++ M4/app/src/main/java/TEAM79b/m4/controller/BadIdScreen.java
@@ -1,30 +1,29 @@
 package TEAM79b.m4.controller;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 
 import TEAM79b.m4.R;
 
-public class BadIdScreen extends AppCompatActivity {
+public class BadIdScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bad_id_screen);
 
-        goToLogin();
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width * .8), 350);
     }
 
-    private void goToLogin() {
-        Button badBack = (Button) findViewById(R.id.badBack);
-        badBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(BadIdScreen.this, LoginScreen.class));
-            }
-        });
-    }
 }
