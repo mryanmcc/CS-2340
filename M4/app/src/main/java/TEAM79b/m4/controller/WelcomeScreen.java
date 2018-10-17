5d644936e28fc824bbc93fb716ab4a31960f6358
rwturko <rwturko@gmail.com>
2018-10-17 02:45:43 -0400

    continued dev on csv parser

--- M4/app/src/main/java/TEAM79b/m4/controller/WelcomeScreen.java
+++ M4/app/src/main/java/TEAM79b/m4/controller/WelcomeScreen.java
@@ -3,10 +3,15 @@ package TEAM79b.m4.controller;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 import TEAM79b.m4.R;
 import TEAM79b.m4.model.Location;
@@ -23,9 +28,22 @@ public class WelcomeScreen extends AppCompatActivity {
 
         Location model = Location.getInstance();
 
-//        try {
-//            InputStream is = getResources().openRawResource()
-//        }
+        try {
+            InputStream locationStream = getResources().openRawResource(R.raw.locationdata);
+            BufferedReader locationStreamBuffer = new BufferedReader(new InputStreamReader(locationStream, StandardCharsets.UTF_8));
+
+            String line;
+            locationStreamBuffer.readLine(); //get rid of header line
+            while ((line = locationStreamBuffer.readLine()) != null) {
+                //Log.d(MainActivity.TAG, line);
+                String[] tokens = line.split(",");
+                int id = Integer.parseInt(tokens[1]);
+                //model.addUser(new String(tokens[NAME_POSITION], tokens[2], id, tokens[3]));
+            }
+            locationStreamBuffer.close();
+        } catch (IOException e) {
+           // Log.e(MainActivity.TAG, "error reading assets", e);
+        }
     }
 
     private void goToLogin() {
