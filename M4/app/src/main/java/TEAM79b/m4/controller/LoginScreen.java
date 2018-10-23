d6e6ad0ec66b9e918678a2e7318567d79a23c48c
rwturko <rwturko@gmail.com>
2018-10-22 23:41:01 -0400

    added pass authe

--- M4/app/src/main/java/TEAM79b/m4/controller/LoginScreen.java
+++ M4/app/src/main/java/TEAM79b/m4/controller/LoginScreen.java
@@ -1,11 +1,20 @@
 package TEAM79b.m4.controller;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import TEAM79b.m4.R;
 import TEAM79b.m4.model.Login;
@@ -16,6 +25,8 @@ public class LoginScreen extends AppCompatActivity {
     private EditText loginPass;
     private Button loginSubmit;
 
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,16 +36,40 @@ public class LoginScreen extends AppCompatActivity {
         loginPass = (EditText) findViewById(R.id.loginPass);
         loginSubmit = (Button) findViewById(R.id.loginSubmit);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         goToWelcome();
     }
 
     public void loginPress(View view) {
-        Login model = Login.getInstance();
-        if (model.getUserAccounts().containsKey(loginEmail.getText().toString()) && model.getUserAccounts().get(loginEmail.getText().toString()).equals(loginPass.getText().toString())) {
-            startActivity(new Intent(LoginScreen.this, AppScreen.class));
-        } else {
-            startActivity(new Intent(LoginScreen.this, BadIdScreen.class));
-        }
+        firebaseAuth.signInWithEmailAndPassword(loginEmail.getText().toString(), loginPass.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            startActivity(new Intent(LoginScreen.this, LocationList.class));
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginScreen.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+//        Login model = Login.getInstance();
+//        if (model.getUserAccounts().containsKey(loginEmail.getText().toString()) && model.getUserAccounts().get(loginEmail.getText().toString()).equals(loginPass.getText().toString())) {
+//            startActivity(new Intent(LoginScreen.this, AppScreen.class));
+//        } else {
+//            startActivity(new Intent(LoginScreen.this, BadIdScreen.class));
+//        }
     }
 
     private void goToWelcome() {
