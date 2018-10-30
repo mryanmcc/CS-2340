2b3bbae56250fbaada84a2f5eb6a79a088bd5740
mryanmcc <mryanmcc@gmail.com>
2018-10-30 17:28:06 -0400

    updating item detail list, fixing location putExtra bug

--- M4/app/src/main/java/TEAM79b/m4/controller/ItemDetailScreen.java
+++ M4/app/src/main/java/TEAM79b/m4/controller/ItemDetailScreen.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.Serializable;
 
@@ -14,12 +15,33 @@ import TEAM79b.m4.model.Location;
 
 public class ItemDetailScreen extends AppCompatActivity implements Serializable {
 
+    private TextView itemTimeStamp;
+    private TextView itemSDesc;
+    private TextView itemLDesc;
+    private TextView itemVal;
+    private TextView itemCat;
+    private TextView itemLoc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_detail_screen);
 
         Item item = (Item) getIntent().getSerializableExtra("itemObj");
+        Location location = (Location) getIntent().getSerializableExtra("locationObj");
+
+        TextView itemTimeStamp = (TextView) findViewById(R.id.itemTimeStamp);
+        itemTimeStamp.setText(item.getTimeStamp());
+        TextView itemSDesc = (TextView) findViewById(R.id.itemSDesc);
+        itemSDesc.setText(item.getShortDesc());
+        TextView itemLDesc = (TextView) findViewById(R.id.itemLDesc);
+        itemLDesc.setText(item.getLongDesc());
+        TextView itemVal = (TextView) findViewById(R.id.itemVal);
+        itemVal.setText("$ " + item.getValue());
+        TextView itemCat = (TextView) findViewById(R.id.itemCat);
+        itemCat.setText(item.getCategory());
+        TextView itemLoc = (TextView) findViewById(R.id.itemLoc);
+        itemLoc.setText("Location: " + location.getName());
 
         goToItemList();
     }
@@ -31,7 +53,7 @@ public class ItemDetailScreen extends AppCompatActivity implements Serializable 
             public void onClick(View view) {
                 Location location = (Location)  getIntent().getSerializableExtra("locationObj");
                 Intent intent = new Intent(ItemDetailScreen.this, ItemListScreen.class);
-                intent.putExtra("LocationObj", location);
+                intent.putExtra("locationObj", location);
                 startActivity(intent);
             }
         });
